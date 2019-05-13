4ba80fb8a0d33e9cb4e024cf2ffbc58245cc38de
milad-kh <milad.khanmohammadi@gmail.com>
2019-05-13 14:10:08 +0430

    products: add elements to create form

--- admin/src/products.js
+++ admin/src/products.js
@@ -1,10 +1,27 @@
 import React from 'react';
-import { List, Create, Edit, SimpleForm, DisabledInput, TextInput, DateInput, LongTextInput, ReferenceManyField, Datagrid, TextField, DateField, EditButton } from 'react-admin';
+import {FileField, AutocompleteArrayInput ,FileInput, ImageField, ImageInput, NumberInput, BooleanInput, DateTimeInput, List, Create, Edit, SimpleForm, DisabledInput, TextInput, DateInput, LongTextInput, ReferenceManyField, Datagrid, TextField, DateField, EditButton } from 'react-admin';
+import RichTextInput from 'ra-input-rich-text';
 
 export const ProductCreate = (props) => (
     <Create {...props}>
         <SimpleForm>
+            <TextInput source="name" />
             <TextInput source="title" />
+            <RichTextInput source="description" />
+            <AutocompleteArrayInput source="tags" choices={[
+                { id: 'programming', name: 'Programming' },
+                { id: 'lifestyle', name: 'Lifestyle' },
+                { id: 'photography', name: 'Photography' },
+            ]} />
+            <NumberInput source="price" />
+            <BooleanInput source="isEnable" />
+            <ImageInput source="thumbnail" label="Related pictures" accept="image/*">
+                <ImageField source="src" title="title" />
+            </ImageInput>
+            <FileInput source="files" label="Related files" accept="application/pdf">
+                <FileField source="file" title="upload file" />
+            </FileInput>
+            <DateTimeInput source="publishedDate" />
         </SimpleForm>
     </Create>
 );
