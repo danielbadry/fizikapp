f63d17b23f2504cff0875faf12b3d3deb6c8003d
milad-kh <milad.khanmohammadi@gmail.com>
2019-06-15 14:21:14 +0430

    products: -- add animation to auto-select-chips-component

--- admin/src/as.js
+++ admin/src/as.js
@@ -1,9 +1,12 @@
 import React from 'react';
 // import Select from 'react-select';
 import Creatable from 'react-select/creatable';
+import makeAnimated from 'react-select/animated';
 
 class MyAutoSelect extends React.Component {
   render() {
+    const animatedComponents = makeAnimated();
+
     const options = [
       { value: 'chocolate', label: 'Chocolate' },
       { value: 'strawberry', label: 'Strawberry' },
@@ -11,6 +14,7 @@ class MyAutoSelect extends React.Component {
     ]
       return (
         <Creatable
+          components={animatedComponents}
           name="colors"
           isMulti
           isClearable
