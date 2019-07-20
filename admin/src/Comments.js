09b0e6c94a510dae22e83b1e3f567dad8406b452
milad-kh <milad.khanmohammadi@gmail.com>
2019-07-20 10:49:03 +0430

    show dialog as a reply container

--- admin/src/Comments.js
+++ admin/src/Comments.js
@@ -8,17 +8,39 @@ import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
 class Comments extends React.Component {
     
     constructor (props) {
         super(props);
+        this.state = {
+          open : false
+        }
     }
     
     componentDidMount () {
        
     }
     
+    handleClickOpen() {
+      this.setState((state, props) => {
+        return {open: true};
+      });
+    }
+    
+    handleClose() {
+      this.setState((state, props) => {
+        return {open: false};
+      });
+    }
+
     render () {
 
       var data = [],
@@ -73,9 +95,40 @@ class Comments extends React.Component {
                             </Typography>
                             
                             {" — 2 days ago - "} 
-                            <Link href="http://www.google.com">
+                            <Link
+                              component="button"
+                              variant="body2"
+                              onClick={this.handleClickOpen.bind(this)}
+                            >
                               reply
                             </Link>
+                            
+                            <Dialog open={this.state.open} onClose={this.handleClose.bind(this)} aria-labelledby="form-dialog-title">
+                              <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+                              <DialogContent>
+                                <DialogContentText>
+                                  To subscribe to this website, please enter your email address here. We will send updates
+                                  occasionally.
+                                </DialogContentText>
+                                <TextField
+                                  autoFocus
+                                  margin="dense"
+                                  id="name"
+                                  label="Email Address"
+                                  type="email"
+                                  fullWidth
+                                />
+                              </DialogContent>
+                              <DialogActions>
+                                <Button onClick={this.handleClose.bind(this)} color="primary">
+                                  Cancel
+                                </Button>
+                                <Button onClick={this.handleClose.bind(this)} color="primary">
+                                  Subscribe
+                                </Button>
+                              </DialogActions>
+                            </Dialog>
+
                             {m.children && <Menu data={m.children} />}
                           </React.Fragment>
                         }
