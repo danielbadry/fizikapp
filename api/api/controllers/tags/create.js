7a689e7bd4f23614d22b39f703ddd01ac52be4c1
milad-kh <milad.khanmohammadi@gmail.com>
2019-08-11 08:14:41 +0430

    enhanced

--- api/api/controllers/tags/create.js
+++ api/api/controllers/tags/create.js
@@ -19,11 +19,11 @@ module.exports = {
   },
   
   fn: async function (inputs) {
-
     return await Tags.create({
       name: inputs.name,
       createdAt : await sails.helpers.dateParse(),
-      updatedAt : await sails.helpers.dateParse()
+      updatedAt : await sails.helpers.dateParse(),
+      count: Math.floor(Math.random() * 10) + 1
     }).fetch();
 
   }
