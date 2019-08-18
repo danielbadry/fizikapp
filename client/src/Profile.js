5c14c87128f3ed230f79acf289984b429e6d12e2
milad-kh <milad.khanmohammadi@gmail.com>
2019-08-18 19:57:55 +0430

    profile

---
+++ client/src/Profile.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import VerticalTabs from './ProfileTabs';
+
+class Profile extends React.Component {
+    render() {
+        return (
+            // <div style={{width:'400px'}}>
+                <VerticalTabs />
+            // </div>
+        );
+    }
+}
+
+export default Profile;
---
+++ client/src/ProfileTabs.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Badge from '@material-ui/core/Badge';
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <Typography
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`vertical-tabpanel-${index}`}
+      aria-labelledby={`vertical-tab-${index}`}
+      {...other}
+    >
+      <Box p={3}>{children}</Box>
+    </Typography>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `vertical-tab-${index}`,
+    'aria-controls': `vertical-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+    display: 'flex',
+    height: 224,
+  },
+  tabs: {
+    borderRight: `1px solid ${theme.palette.divider}`,
+  },
+}));
+
+export default function VerticalTabs() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  function handleChange(event, newValue) {
+    setValue(newValue);
+  }
+
+  return (
+    <div className={classes.root}>
+      <Tabs
+        orientation="vertical"
+        variant="scrollable"
+        value={value}
+        onChange={handleChange}
+        aria-label="Vertical tabs example"
+        className={classes.tabs}
+      >
+        <Tab label="Basics" {...a11yProps(0)} />
+        <Tab label="Financial" {...a11yProps(1)} />
+        <Tab label="Watched videos" {...a11yProps(2)} />
+        <Tab label="Settings" {...a11yProps(3)} />
+        <Tab label={
+              <Badge className={classes.padding} color="secondary" badgeContent={4}>
+                  Inbox
+              </Badge>
+            } {...a11yProps(4)} />
+      </Tabs>
+      <TabPanel value={value} index={0}>
+        Basics
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Financial
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Watched videos
+      </TabPanel>
+      <TabPanel value={value} index={3}>
+        Settings
+      </TabPanel>
+      <TabPanel value={value} index={3}>
+        Inbox
+      </TabPanel>
+    </div>
+  );
+}
