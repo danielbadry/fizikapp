25963dbfc5f29e5b674f7ed848bb7f466cfec2e9
milad-kh <milad.khanmohammadi@gmail.com>
2019-08-26 10:14:39 +0430

    quiz in product

--- client/src/QuizComponent.js
+++ client/src/QuizComponent.js
@@ -13,13 +13,21 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import TextField from '@material-ui/core/TextField';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
 
 class QuizComponent extends React.Component {
     
     constructor(props) {
         super(props);
         this.state = {
-            isQuizDialogOpen: false
+            isQuizDialogOpen: false,
+            quizes : [],
+            step: 0
         }
     }
 
@@ -38,13 +46,26 @@ class QuizComponent extends React.Component {
             .then(response => response.json())
             .then(quizes => {
                 this.setState({
-                    
+                   quizes: quizes 
                 });
+                console.info('hello:', quizes);
             });
     }
 
     handleDialogStatus = () => {
         this.setState({isQuizDialogOpen: !(this.state.isQuizDialogOpen)});
+    }
+
+    checkAndGoNextStep = () => {
+        this.setState({
+            step: (this.state.step + 1)
+        });
+    }
+    
+    checkAndGoBackStep = () => {
+        this.setState({
+            step: (this.state.step - 1)
+        });
     }
 
     render() {
@@ -66,20 +87,50 @@ class QuizComponent extends React.Component {
                     >
                     <DialogTitle 
                         id="simple-dialog-title"
-                        >Set backup account
+                        >quiz of this product
                     </DialogTitle>
                     <DialogContent>
                     <DialogContentText>
-                        To subscribe to this website, please enter your email address here. We will send updates
-                        occasionally.
+                        {this.state.quizes.map((quiz, index) => 
+                            (index==this.state.step?<div
+                                key={index}
+                                >
+                                    <div>{quiz.question}</div>
+                                    <div>
+                                        {quiz.options.map(
+                                            (option, ind) =>
+                                            <RadioGroup
+                                                aria-label="gender"
+                                                name="gender1"
+                                                // className={classes.group}
+                                                // value={value}
+                                                // onChange={handleChange}
+                                                >
+                                                <FormControlLabel value="female" control={<Radio />} label={option.title} />
+                                                
+                                            </RadioGroup>
+                                        )}
+                                    </div>
+                                </div>:
+                                <div></div>
+                                )
+                            
+                            
+                        )}
                     </DialogContentText>
                     
                     </DialogContent>
                     <DialogActions>
-                    <Button color="primary">
+                    <Button 
+                        color="primary"
+                        onClick={this.checkAndGoBackStep}
+                        >
                         Back
                     </Button>
-                    <Button color="primary">
+                    <Button 
+                        color="primary"
+                        onClick={this.checkAndGoNextStep}
+                        >
                         Next
                     </Button>
                     </DialogActions>
