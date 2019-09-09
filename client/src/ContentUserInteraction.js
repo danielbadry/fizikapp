4d8f5fff8d42f05841954e7366a5702100979be2
milad-kh <milad.khanmohammadi@gmail.com>
2019-09-09 16:22:56 +0430

    Userinteraction component

--- client/src/ContentUserInteraction.js
+++ client/src/ContentUserInteraction.js
@@ -8,7 +8,6 @@ import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import UserInteractionNode from './UserInteractionNode';
-import ProductsComments from './ProductsComments';
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -112,10 +111,6 @@ export default function ContentUserInteraction(props) {
               </TabPanel>
           )
         }
-        
-        {/* <TabPanel value={value} index={1} dir={theme.direction}>
-          <ProductsComments />
-        </TabPanel> */}
         
       </SwipeableViews>
 
