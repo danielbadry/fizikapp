2671af9e082f3ffee189f91f042e9accb2f0e1af
milad-kh <milad.khanmohammadi@gmail.com>
2019-10-21 16:12:59 +0330

    work on appbar

--- client/src/UserToolInAppbar.js
+++ client/src/UserToolInAppbar.js
@@ -1,16 +1,8 @@
 import React from 'react';
-import IconButton from '@material-ui/core/IconButton';
-import Badge from '@material-ui/core/Badge';
-import MailIcon from '@material-ui/icons/Mail';
-import NotificationsIcon from '@material-ui/icons/Notifications';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import Typography from '@material-ui/core/Typography';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
-import Button from '@material-ui/core/Button';
 import UserProfileMenu from './UserProfileMenu';
 import UserNotificationMenu from './UserNotificationMenu';
 import UserSystemMessageMenu from './UserSystemMessageMenu';
+import Typography from '@material-ui/core/Typography';
 
 class UserToolInAppbar extends React.Component {
     
@@ -52,16 +44,7 @@ class UserToolInAppbar extends React.Component {
         if (this.state.userinfo)
             return(
                 <div>
-                    {/* <IconButton aria-label="show 4 new mails" color="inherit">
-                    <Badge badgeContent={4} color="secondary">
-                        <MailIcon />
-                    </Badge>
-                    </IconButton>
-                    <IconButton aria-label="show 17 new notifications" color="inherit">
-                    <Badge badgeContent={17} color="secondary">
-                        <NotificationsIcon />
-                    </Badge>
-                    </IconButton> */}
+                    {this.state.userinfo.firstName + ' ' +this.state.userinfo.lastName} خوش آمدید
                     <UserSystemMessageMenu />
                     <UserNotificationMenu />
                     <UserProfileMenu />
