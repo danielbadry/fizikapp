445222a310736244cc269f3cb44956145f24fb20
milad-kh <milad.khanmohammadi@gmail.com>
2019-11-01 16:35:00 +0330

    Profile layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import Routs from './Routs';
 import './font-style.css';
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4charts from "@amcharts/amcharts4/charts";
+import am4themes_animated from "@amcharts/amcharts4/themes/animated";
 
+am4core.useTheme(am4themes_animated);
 export default function App() {
 
   return (
--- client/src/Profile.js
+++ client/src/Profile.js
@@ -2,28 +2,137 @@ import React from 'react';
 import ProfileTabs from './ProfileTabs';
 import Grid from '@material-ui/core/Grid';
 import MainHeader from "./MainHeader";
-import MainFooter from "./MainFooter";
+import ChartExample1 from "./ChartExample1";
+import StickyFooter from "./StickyFooter";
+import Paper from '@material-ui/core/Paper';
+import { Typography } from '@material-ui/core';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+import InboxIcon from '@material-ui/icons/Inbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
 
 class Profile extends React.Component {
     render() {
         return (
-            <div>
-                <Grid container spacing={3}>
-                        
-                    <Grid item xs={12}>
-                        <MainHeader />
+            <React.Fragment>
+
+                <MainHeader />
+
+                <Grid container spacing={2} justify="center">
+
+                    <Grid item xs={8} sm={8} md={8} lg={8} xl={8}>
+                        <Paper>
+                            <Grid container spacing={2} justify="center" >
+
+                               <Grid item xs={3} sm={3} md={3} lg={3} xl={3} >
+                                    <Paper>
+                                      B1 
+                                    </Paper> 
+                               </Grid>
+
+                               <Grid item xs={3} sm={3} md={3} lg={3} xl={3} >
+                                    <Paper>
+                                      B2 
+                                    </Paper>
+                               </Grid>
+
+                               <Grid item xs={3} sm={3} md={3} lg={3} xl={3} >
+                                    <Paper>
+                                      B3 
+                                    </Paper>
+                               </Grid>
+                               
+                               <Grid item xs={3} sm={3} md={3} lg={3} xl={3} >
+                                    <ChartExample1 />
+                               </Grid>
+
+                            </Grid>
+                        </Paper>
                     </Grid>
                     
-                    <Grid item xs={12}>
-                        <ProfileTabs />
+                    <Grid item xs={2} sm={2} md={2} lg={2} xl={2}>
+                        <Paper>
+                            <Grid container spacing={0}>
+                                
+                                <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                                    <img src="https://www.goldenglobes.com/sites/default/files/styles/portrait_medium/public/gallery_images/17-tomcruiseag.jpg?itok=qNj0cQGV&c=c9a73b7bdf609d72214d226ab9ea015e" />
+                                </Grid>
+                                
+                                <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                                    <Typography
+                                        style={{
+                                            fontFamily:'IranSans'
+                                        }}
+                                        >
+                                        ایمان ارقامی 
+                                    </Typography>
+                                    <Typography 
+                                        style={{
+                                            fontFamily:'IranSans'
+                                        }}>
+                                        مدیر سیستم
+                                    </Typography>
+                                </Grid>
+                                
+                                <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                                    <hr />
+                                </Grid>
+                                
+                                <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                                    <List component="nav" aria-label="main mailbox folders" style={{
+                                        direction: 'rtl'
+                                    }}>
+                                        <ListItem button>
+                                        <ListItemIcon>
+                                            <InboxIcon />
+                                        </ListItemIcon>
+                                        <ListItemText 
+                                            primary={<Typography type="body2" style={{ textAlign: 'right', fontFamily:'IranSans', fontSize:'14px' }}>عمومی</Typography>} />
+                                        </ListItem>
+                                        <ListItem button>
+                                        <ListItemIcon>
+                                            <DraftsIcon />
+                                        </ListItemIcon>
+                                        <ListItemText primary={<Typography type="body2" style={{ textAlign: 'right', fontFamily:'IranSans', fontSize:'14px' }}>مالی</Typography>} />
+                                        </ListItem>
+                                        <ListItem button>
+                                        <ListItemIcon>
+                                            <DraftsIcon />
+                                        </ListItemIcon>
+                                        <ListItemText primary={<Typography type="body2" style={{ textAlign: 'right', fontFamily:'IranSans', fontSize:'14px' }}>درخواست های من</Typography>} />
+                                        </ListItem>
+                                    </List>
+                                </Grid>
+
+                            </Grid>
+                        </Paper>
                     </Grid>
-                    
-                    <Grid container>
-                        <MainFooter />
-                    </Grid>
-                    
+
                 </Grid>
-            </div>
+
+                <StickyFooter />
+
+            </React.Fragment>
+            // <div>
+            //     <Grid container spacing={3}>
+                        
+            //         <Grid item xs={12}>
+            //             <MainHeader />
+            //         </Grid>
+                    
+            //         <Grid item xs={12}>
+            //             <ProfileTabs />
+            //         </Grid>
+                    
+            //         <Grid container>
+            //             <MainFooter />
+            //         </Grid>
+                    
+            //     </Grid>
+            // </div>
         );
     }
 }
