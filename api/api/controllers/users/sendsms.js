cfedfe8a7f3f2c261950f1834cc365311d10fb4e
milad-kh <milad.khanmohammadi@gmail.com>
2019-11-08 00:40:18 +0330

    -- send SMS

---
+++ api/api/controllers/users/sendsms.js
@@ -0,0 +1,54 @@
+var soap = require('soap');
+module.exports = {
+
+
+  friendlyName: 'Sendmail',
+
+
+  description: 'Sendmail users.',
+
+
+  inputs: {
+
+  },
+
+
+  exits: {
+
+  },
+
+
+  fn: async function (inputs) {
+    var url = 'http://sms.hostiran.net/webservice/?WSDL';
+    var httpOptions = {
+      wsdl_headers: {
+        'Authorization': 'Basic ' + Buffer.from('miladkhan' + ':' + '789654').toString('base64')
+      }
+    };
+    
+
+    soap.createClient(url, httpOptions, function(err, client) {
+      if (err) {
+        response.status(401).end();
+      } else {
+        var requestArgs = {
+          to:'09380331526',
+          msg:'سلام'
+        };
+
+        client.addHttpHeader('Authorization', "Basic " + Buffer.from('miladkhan' + ':' + '789654').toString('base64'));
+        // client.send(requestArgs);
+        client.send(requestArgs, function(err, result) {
+          if (err) {
+            console.log(err.message);
+          }
+    
+          // console.log('i found ' + result);
+          // response.send(result);
+        });
+      }
+    });
+
+  }
+
+};
