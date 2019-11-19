8250ac485e1cce1b33a1f7fb5e4b95c1bfed2597
milad-kh <milad.khanmohammadi@gmail.com>
2019-11-19 14:37:36 +0330

    Category

--- client/src/Category.js
+++ client/src/Category.js
@@ -11,168 +11,42 @@ class Category extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
-            category : []
+            
         }
     }
 
     componentDidMount() {
-        const token = localStorage.getItem('token');
-        fetch(process.env.REACT_APP_API_URL+`categories/?rowId=${this.props.match.params.categoryid}`, {
-            method: 'GET', 
-            mode: 'cors',
-            cache: 'no-cache',
-            credentials: 'same-origin',
-            headers: {
-                'Content-Type': 'application/json',
-                'authorization': `Bearer ${token}`,
-            },
-            redirect: 'follow',
-            referrer: 'no-referrer',
-            })
-            .then(response => response.json())
-            .then(category => {
-                this.setState(function(state, props) {
-                    return {
-                        category: category
-                        }
-                  });
-            });
+        
     }
     
     componentDidUpdate(prevProps, prevState) {
-        if(prevProps.match.params.categoryid != this.props.match.params.categoryid) {
-        const token = localStorage.getItem('token');
-        fetch(process.env.REACT_APP_API_URL+`categories/?rowId=${this.props.match.params.categoryid}`, {
-            method: 'GET', 
-            mode: 'cors',
-            cache: 'no-cache',
-            credentials: 'same-origin',
-            headers: {
-                'Content-Type': 'application/json',
-                'authorization': `Bearer ${token}`,
-            },
-            redirect: 'follow',
-            referrer: 'no-referrer',
-            })
-            .then(response => response.json())
-            .then(category => {
-                this.setState(function(state, props) {
-                    return {
-                        category: category
-                        }
-                  });
-            });
-        }
+        
     }
 
     render() {
         return(
             <React.Fragment>
-                {this.state.category.map(
-                    (item, index) => 
-                        <Paper
-                            key={index}
-                            style={{
-                                direction: 'rtl',
-                                fontFamily:'IranSans'
-                            }}
-                            >
-                            <Grid container spacing={0}>
-                                <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-                                    <Link 
-                                        color="inherit"
-                                        style={{
-                                            fontFamily: 'IranSans_Ultralight',
-                                            fontSize: '13px',
-                                            margin:'0',
-                                            lineHeight:'2'
-                                        }}
-                                        component={RouterLink} 
-                                        to={`/category/${item.id}`}>
-                                            {item.name}
-                                    </Link>
-                                </Grid>
-
-                                {item.allSubCategories.map(
-                                    (category, cIndex) => 
-                                        <Grid key={cIndex} item xs={12} sm={12} md={12} lg={12} xl={12}>
-                                            <Grid container spacing={0}>
-                                                <Grid item xs={10} sm={10} md={10} lg={10} xl={10}>
-                                                    <Grid container spacing={0}>
-                                                        <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-                                                            <Link 
-                                                                color="inherit"
-                                                                style={{
-                                                                    fontFamily: 'IranSans_Ultralight',
-                                                                    fontSize: '13px',
-                                                                    margin:'0',
-                                                                    lineHeight:'2'
-                                                                }}
-                                                                component={RouterLink} 
-                                                                to={`/category/${category.id}`}>
-                                                                   {category.name}
-                                                            </Link>
-                                                        </Grid>
-                                                        <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-                                                            {category.description}
-                                                        </Grid>
-                                                    </Grid>
-                                                </Grid>
-                                                <Grid item xs={2} sm={2} md={2} lg={2} xl={2}>
-                                                    <img 
-                                                        style={{
-                                                            width:'50px'
-                                                        }}
-                                                        src={category.thumbnail} 
-                                                        />
-                                                </Grid>
-                                            </Grid>
-                                        </Grid>
-                                )}
-
-                                {item.Products.map(
-                                    (product, pIndex) => 
-                                        <Grid key={pIndex} item xs={12} sm={12} md={12} lg={12} xl={12}>
-                                            <Grid container spacing={0}>
-                                                <Grid item xs={10} sm={10} md={10} lg={10} xl={10}>
-                                                    <Grid container spacing={0}>
-                                                        <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-                                                            <Link 
-                                                                color="inherit"
-                                                                style={{
-                                                                    fontFamily: 'IranSans_Ultralight',
-                                                                    fontSize: '13px',
-                                                                    margin:'0',
-                                                                    lineHeight:'2'
-                                                                }}
-                                                                component={RouterLink} 
-                                                                to={`/product/${product.id}`}>
-                                                                   {product.name}
-                                                            </Link>
-                                                        </Grid>
-                                                        <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-                                                            {product.description}
-                                                        </Grid>
-                                                    </Grid>
-                                                </Grid>
-                                                <Grid item xs={2} sm={2} md={2} lg={2} xl={2}>
-                                                    <img 
-                                                        style={{
-                                                            width:'50px'
-                                                        }}
-                                                        src={product.thumbnail} 
-                                                        />
-                                                </Grid>
-                                            </Grid>
-                                        </Grid>
-                                )}
-                                
-                                
-                            </Grid>
-            
+                <Grid container justify="center" spacing={1}>
+                    <Grid item item xs={3} sm={3} md={3} lg={3} xl={3}>
+                        <Paper>
+                            tree
                         </Paper>
-                )}
-            
+                    </Grid>
+                    <Grid item xs={8} sm={8} md={8} lg={8} xl={8}>
+                        <Paper>
+                            {[1,2,3].map(
+                                (item, index) =>
+                                    <Grid 
+                                        style={{
+                                            direction:'rtl'
+                                        }}
+                                        item xs={12} sm={12} md={12} lg={12} xl={12}>
+                                        item
+                                    </Grid>
+                            )}
+                        </Paper>
+                    </Grid>
+                </Grid>
             </React.Fragment>
         )
     }
