5e2dd008533373bddbcf649b7c768f62684c6e1f
milad-kh <milad.khanmohammadi@gmail.com>
2019-12-10 22:50:19 +0330

    enhance

--- api/api/controllers/criticisms/find.js
+++ api/api/controllers/criticisms/find.js
@@ -48,36 +48,57 @@ module.exports = {
     let criticismWithNoResponse = [];
     let criticismWithResponse = [];
     for (let criticism of allCriticisms) {
-      if (criticism.response == '')
-        criticismWithNoResponse.push(criticism);
-      else 
-        criticismWithResponse.push(criticism);
+      if (criticism.response === '')
+      {criticismWithNoResponse.push(criticism);}
+      else
+      {criticismWithResponse.push(criticism);}
     }
     allCriticisms = criticismWithNoResponse.concat(criticismWithResponse);
     
-
-
     for (let criticism of allCriticisms) { 
-      let user = await Users.find({
-        where : {
+      if (criticism.userId) {
+        let user = await Users.findOne({
           id: criticism.userId,
           isDeleted: false
+        });
+       
+        criticism.userInfo = user;
+        criticism.userInfo.fullName = criticism.userInfo.firstName + ' ' + criticism.userInfo.lastName;
+        criticism.thumbnail = "http://localhost:1337/files/usersImage/" + criticism.userInfo.thumbnail;
+        if(criticism.response.length === 0) {
+          criticism.isResponsed = false;
         }
-      });
-      user = user[0];
-      criticism.userInfo = user;
-      criticism.userInfo.fullName = criticism.userInfo.firstName + ' ' + criticism.userInfo.lastName;
-      criticism.thumbnail = "http://localhost:1337/files/usersImage/" + criticism.userInfo.thumbnail;
-      if(criticism.response.length == 0)
-        criticism.isResponsed = false;
-      else
-        criticism.isResponsed = true;
-      moment.locale('en');
-      criticism.jalaaliCreatedDate = momentJalaali(criticism.createdAt, 'YYYY-M-D HH:mm:ss').format('jYYYY/jM/jD HH:mm:ss');
-      moment.locale('fa');
-      criticism.jalaaliUserFriendlyCreatedDate = moment(criticism.createdAt).fromNow();
-      criticism.jalaaliFullUserFriendlyCreatedDate = criticism.jalaaliCreatedDate + ' ' + criticism.jalaaliUserFriendlyCreatedDate;
+          
+        else {
+          criticism.isResponsed = true;
+        }
+          
+        moment.locale('en');
+        criticism.jalaaliCreatedDate = momentJalaali(criticism.createdAt, 'YYYY-M-D HH:mm:ss').format('jYYYY/jM/jD HH:mm:ss');
+        moment.locale('fa');
+        criticism.jalaaliUserFriendlyCreatedDate = moment(criticism.createdAt).fromNow();
+        criticism.jalaaliFullUserFriendlyCreatedDate = criticism.jalaaliCreatedDate + ' ' + criticism.jalaaliUserFriendlyCreatedDate;
+      } else {
+        criticism.userInfo = {};
+        criticism.userInfo.fullName = criticism.fullName;
+        criticism.thumbnail = "http://localhost:1337/files/usersImage/" + criticism.userInfo.thumbnail;
+        if(criticism.response.length === 0) {
+          criticism.isResponsed = false;
+        }
+          
+        else {
+          criticism.isResponsed = true;
+        }
+          
+        moment.locale('en');
+        criticism.jalaaliCreatedDate = momentJalaali(criticism.createdAt, 'YYYY-M-D HH:mm:ss').format('jYYYY/jM/jD HH:mm:ss');
+        moment.locale('fa');
+        criticism.jalaaliUserFriendlyCreatedDate = moment(criticism.createdAt).fromNow();
+        criticism.jalaaliFullUserFriendlyCreatedDate = criticism.jalaaliCreatedDate + ' ' + criticism.jalaaliUserFriendlyCreatedDate;
+      }
+      
     }
+
     finalData.dataLength = dataLength.length;
     finalData.data = allCriticisms;
     finalData.isAuthenticated = true;
