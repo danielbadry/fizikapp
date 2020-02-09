0cd533973fa178e4853ebbb304313106e806c903
Mahdi Bagheri <mahdifox2000@gmail.com>
2020-02-09 10:59:57 +0330

    delete react-native from master branch

--- physicvideo/src/ggn_config.js
+++
@@ -1,6 +0,0 @@
-export default {
-	name : 'فیزیک اپ',
-	dir  : 'rtl',
-	lng	 : 'fa',
-	url  : 'https://fizik.app/api',
-};
--- physicvideo/src/i18n/ggn_translate.js
+++
@@ -1,7 +0,0 @@
-const ggn_translate = (obj,verb) => {
-	if(obj.hasOwnProperty(verb)){
-		return obj[verb];
-	}
-	return verb;
-};
-export default ggn_translate
